a020963b69487e4b23012431a17c81cfafd09f48
TaraSalli <108293185+TaraSalli@users.noreply.github.com>
2023-04-18 08:57:51 -0400

    initial commit

---
+++ NimRunner.java
@@ -0,0 +1,21 @@
+public class Minimax {
+    
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static void runGame() {
+        
+    }
+
+    public static void getXMove() {
+
+    }
+
+    public static void getXYove() {
+
+    }
+
+
+}
